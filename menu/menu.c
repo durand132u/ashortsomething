f5d64f08c76eaf8323d2e61e2bfbb016b9e86319
durand132u <felixdurand@orange.fr>
2018-10-22 17:01:11 +0200

    affichage dun menu seul pour commencer

---
+++ menu/menu.c
@@ -0,0 +1,14 @@
+#include <SDL.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 1024
+
+
+
+int colorkey;
+
