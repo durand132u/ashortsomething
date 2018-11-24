a2124f53af92107cc04de33a8b466e62c2d8869b
durand132u <felixdurand@orange.fr>
2018-11-24 20:53:49 +0100

    Upload menu qui marche et compile

--- sdlanim.c
+++ sdlanim.c
@@ -52,7 +52,7 @@ int choiceTEST = 0;
    We also change the animation bit used for creating the "walk" effect.
 */
 void HandleEvent(SDL_Event event, int *gameover, int *currDirection, int *animFlip, struct sprite_t *perso, struct sprite_t *ludo,
- struct bdf_t *fireball, struct sprite_t *poisonball, struct sprite_t *deathball, struct sprite_t *HP_potion,int *display, int *posMouseX, int *posMouseY)
+ struct bdf_t *fireball, struct sprite_t *poisonball, struct sprite_t *deathball, struct sprite_t *HP_potion,int *display, int *posMouseX, int *posMouseY, int *selection)
 {
 	switch (event.type) {
 		/* close button clicked */
@@ -64,9 +64,9 @@ void HandleEvent(SDL_Event event, int *gameover, int *currDirection, int *animFl
 		
 		case SDL_KEYDOWN:
 			switch (event.key.keysym.sym) {
-				case SDLK_ESCAPE:   
+				case SDLK_ESCAPE:   // retour au menu
 				case SDLK_q:
-					*gameover = 0;
+					*display =1 ;
 					break;
 			  
 				case SDLK_LEFT:
@@ -233,6 +233,23 @@ void HandleEvent(SDL_Event event, int *gameover, int *currDirection, int *animFl
 				*gameover=0;
 			}
 			break;
+			
+		case SDL_MOUSEMOTION:
+			*selection=-1; //reset
+			if((*display==1)){
+				SDL_GetMouseState(posMouseX,posMouseY);
+			}
+			if((*posMouseX>=800)&&(*posMouseX<=925)&&(*posMouseY>=150)&&(*posMouseY<=200)&&(*display==1)){
+				*selection=1; //play
+			}
+			if((*posMouseX>=800)&&(*posMouseX<=980)&&(*posMouseY>=300)&&(*posMouseY<=350)&&(*display==1)){
+				*selection=2; //options
+			}
+			if((*posMouseX>=800)&&(*posMouseX<=925)&&(*posMouseY>=450)&&(*posMouseY<=500)&&(*display==1)){
+				*selection=3; //quit
+			}
+			break;
+				
 		break;
 	}
 }
@@ -335,10 +352,11 @@ int main(int argc, char* argv[]){
 	
 	//Les surfaces du menu
 	SDL_Surface *background; 
-	SDL_Surface *message; 
-	SDL_Surface *message2; 
-	SDL_Surface *message3; 
-	SDL_Surface *message4; 
+	SDL_Surface *message;
+	SDL_Surface *fleche;
+	int selection=-1;
+	SDL_Rect posMes;
+	SDL_Rect posFleche;
 
 	//Les Fonts qu'on va utiliser pour le menu
 	TTF_Font *font50; 
@@ -407,14 +425,6 @@ int main(int argc, char* argv[]){
 	}
 	
 	//Mise en place du ttf
-	message = TTF_RenderText_Solid(font50,"Welcome to AShortSomething !",textColor);
-	SDL_Rect posMes1={50*SCREEN_WIDTH/100-400,0.0};
-	message2 = TTF_RenderText_Solid(font36,"Play",textColor);
-	SDL_Rect posMes2={80*SCREEN_WIDTH/100,20*SCREEN_HEIGHT/100};
-	message3 = TTF_RenderText_Solid(font36,"Options",textColor);
-	SDL_Rect posMes3={80*SCREEN_WIDTH/100,40*SCREEN_HEIGHT/100};
-	message4 = TTF_RenderText_Solid(font36,"Quit",textColor);
-	SDL_Rect posMes4={80*SCREEN_WIDTH/100,60*SCREEN_HEIGHT/100};
 	
 	//Positions souris
 	int posMouseX=0;
@@ -471,7 +481,7 @@ int main(int argc, char* argv[]){
 		/* look for an event; possibly update the position and the shape
 		 * of the sprite. */
 		if (SDL_PollEvent(&event)) {
-			HandleEvent(event, &gameover, &currentDirection, &animationFlip, &perso, &ludo, &fireball, &poisonball, &deathball, &HP_potion,&display,&posMouseX,&posMouseY);
+			HandleEvent(event, &gameover, &currentDirection, &animationFlip, &perso, &ludo, &fireball, &poisonball, &deathball, &HP_potion,&display,&posMouseX,&posMouseY,&selection);
 		}
 			
 		//Barre de vie ludo
@@ -731,7 +741,7 @@ int main(int argc, char* argv[]){
 		}
 			
 		/* draw the background */
-		if(disp==1){
+		if((disp==1)||(disp==3)){
 			SDL_BlitSurface(background,NULL,screen,NULL);
 		}
 		if(disp==2){
@@ -889,16 +899,56 @@ int main(int argc, char* argv[]){
 			
 			if(Collision(&HP_potion,&ludo)&&HP_potion.display!=0&&ludo.life<MAX_HP){
 				HP_potion.display=0;
-				ludo.life +=50;
+				if(ludo.life+50<MAX_HP){ //Debug des pvs superieur au max en cas de potion ramassee
+					ludo.life +=50;
+				}else{
+					ludo.life=MAX_HP;
+				}
 				printf("Vie de ludo : %d \n", ludo.life);
 			}
 		}
 		/* update the screen */
 		if(disp==1){
-			SDL_BlitSurface(message,NULL,screen,&posMes1);
-			SDL_BlitSurface(message2,NULL,screen,&posMes2);
-			SDL_BlitSurface(message3,NULL,screen,&posMes3);
-			SDL_BlitSurface(message4,NULL,screen,&posMes4);
+			message = TTF_RenderText_Solid(font50,"Welcome to AShortSomething !",textColor);
+			posMes.x=50*SCREEN_WIDTH/100-400;
+			posMes.y=0.0;
+			SDL_BlitSurface(message,NULL,screen,&posMes);
+			message = TTF_RenderText_Solid(font36,"Play",textColor);
+			posMes.x=80*SCREEN_WIDTH/100;
+			posMes.y=20*SCREEN_HEIGHT/100;
+			SDL_BlitSurface(message,NULL,screen,&posMes);
+			message = TTF_RenderText_Solid(font36,"Options",textColor);
+			posMes.x=80*SCREEN_WIDTH/100;
+			posMes.y=40*SCREEN_HEIGHT/100;
+			SDL_BlitSurface(message,NULL,screen,&posMes);
+			message = TTF_RenderText_Solid(font36,"Quit",textColor);
+			posMes.x=80*SCREEN_WIDTH/100;
+			posMes.y=60*SCREEN_HEIGHT/100;
+			SDL_BlitSurface(message,NULL,screen,&posMes);
+			if(selection!=-1){
+				fleche = TTF_RenderText_Solid(font50,">>>",textColor);
+				posFleche.x=65*SCREEN_WIDTH/100;
+				posFleche.y=((20*SCREEN_HEIGHT/100)*selection)-(2*SCREEN_HEIGHT/100);
+				SDL_BlitSurface(fleche,NULL,screen,&posFleche);
+			}
+		}
+		if(disp==3){
+			message = TTF_RenderText_Solid(font50,"OPTIONS :",textColor);
+			posMes.x=50*SCREEN_WIDTH/100-100;
+			posMes.y=0.0;
+			SDL_BlitSurface(message,NULL,screen,&posMes);
+			message = TTF_RenderText_Solid(font36,"Agrandir la taille de l'ecran",textColor);
+			posMes.x=5*SCREEN_WIDTH/100;
+			posMes.y=40*SCREEN_HEIGHT/100;
+			SDL_BlitSurface(message,NULL,screen,&posMes);
+			message = TTF_RenderText_Solid(font36,"Reduire la taille de l'ecran",textColor);
+			posMes.x=5*SCREEN_WIDTH/100;
+			posMes.y=60*SCREEN_HEIGHT/100;
+			SDL_BlitSurface(message,NULL,screen,&posMes);
+			message = TTF_RenderText_Solid(font36,"Changer les controles",textColor);
+			posMes.x=5*SCREEN_WIDTH/100;
+			posMes.y=80*SCREEN_HEIGHT/100;
+			SDL_BlitSurface(message,NULL,screen,&posMes);
 		}
         SDL_UpdateRect(screen,0,0,0,0);
 		SDL_Delay(12);
@@ -918,9 +968,7 @@ int main(int argc, char* argv[]){
 		SDL_FreeSurface(background);
 		SDL_FreeSurface(screen);
 		SDL_FreeSurface(message);
-		SDL_FreeSurface(message2);
-		SDL_FreeSurface(message3);
-		SDL_FreeSurface(message4);
+		SDL_FreeSurface(fleche);
 		SDL_Quit();
 
 		return 0;
