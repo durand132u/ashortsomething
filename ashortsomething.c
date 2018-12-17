e7f79878528d3ea64fd5d7dfd6ad102bb82ea3f0
durand132u <felixdurand@orange.fr>
2018-12-17 10:46:03 +0100

    suppr dinclude

--- ashortsomething.c
+++ ashortsomething.c
@@ -1,14 +1,4 @@
-#include <SDL.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <math.h>
-#include <time.h>
-#include <SDL_ttf.h>
-#include <SDL/SDL_mixer.h>
-#include "game.h"
 #include "sdlanim.c"
-
 int main(){
     zeldaMusique();
 	rungame();
